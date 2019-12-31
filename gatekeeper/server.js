c1ebad7d48e11dd194a0f05ca0fa46f6e526bf83
JoshuaManto <joshuamanto@yahoo.com>
2019-12-31 12:18:13 -0800

    A graphql server using express and express-graphql

---
+++ gatekeeper/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const graphqlHTTP = require("express-graphql");
+const bodyParser = require("body-parser");
+
+const schema = require("./schema.js");
+const resolver = require("./resolver.js");
+
+const PORT = 3000;
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    rootValue: resolver,
+    graphiql: true
+  })
+);
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}...`);
+});
+
+module.exports = app;
